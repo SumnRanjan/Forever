ff172f3d3dca786f19c664e611b21d7bac3b19ef
Suman Ranjan <sumanranjan17291729@gmail.com>
2024-12-03 20:56:37 +0530

    upto about page '

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,10 +5,12 @@ import { ShopContext } from "../context/ShopContext";
 
 const Navbar = () => {
   const [visible, setVisible] = useState(false);
-  const {setShowSearch , getCartCount} = useContext(ShopContext)
+  const { setShowSearch, getCartCount } = useContext(ShopContext);
   return (
-    <div className="flex items-center justify-between py-5 font-medium"> 
-      <Link to={'/'}><img src={assets.logo} alt="" className="w-36" /></Link>
+    <div className="flex items-center justify-between py-5 font-medium">
+      <Link to={"/"}>
+        <img src={assets.logo} alt="" className="w-36" />
+      </Link>
 
       <ul className="hidden sm:flex gap-5 text-sm text-gray-700 ">
         <Link to="/" className="flex flex-col items-center gap-1">
@@ -33,14 +35,21 @@ const Navbar = () => {
       </ul>
 
       <div className="flex items-center gap-6">
-        <img onClick={()=> setShowSearch(true)} src={assets.search_icon} className="w-5 cursor-pointer" alt="" />
+        <img
+          onClick={() => setShowSearch(true)}
+          src={assets.search_icon}
+          className="w-5 cursor-pointer"
+          alt=""
+        />
 
         <div className="relative group">
-          <img
-            src={assets.profile_icon}
-            className="w-5 cursor-pointer"
-            alt=""
-          />
+          <Link to={'/login'}>
+            <img
+              src={assets.profile_icon}
+              className="w-5 cursor-pointer"
+              alt=""
+            />
+          </Link>
           <div className="hidden group-hover:block absolute right-0 pt-4">
             <div className="flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded shadow-lg">
               <p className="cursor-pointer hover:text-black">My Profile</p>
@@ -66,18 +75,48 @@ const Navbar = () => {
       {/*  Side menu for small screens*/}
 
       <div
-        className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-white transition-all ${visible ? "w-full" : "w-0"}`}>
-          <div className="flex flex-col text-gray-600">
-                <div onClick={() => setVisible(false)} className="flex items-center gap-4 p-3 cursor-pointer">
-                      <img src={assets.dropdown_icon}  className="h-4 rotate-180" alt="" />
-                      <p>Back</p>
-                </div>
-                <Link onClick={()=> setVisible(false)} className='py-2 pl-6 border' to='/'>HOME</Link>
-                <Link onClick={()=> setVisible(false)} className='py-2 pl-6 border' to='/collection'>COLLECTION</Link>
-                <Link onClick={()=> setVisible(false)} className='py-2 pl-6 border' to='/about'>ABOUT</Link>
-                <Link onClick={()=> setVisible(false)} className='py-2 pl-6 border' to='/contact'>CONTACT</Link>
+        className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-white transition-all ${
+          visible ? "w-full" : "w-0"
+        }`}
+      >
+        <div className="flex flex-col text-gray-600">
+          <div
+            onClick={() => setVisible(false)}
+            className="flex items-center gap-4 p-3 cursor-pointer"
+          >
+            <img src={assets.dropdown_icon} className="h-4 rotate-180" alt="" />
+            <p>Back</p>
           </div>
+          <Link
+            onClick={() => setVisible(false)}
+            className="py-2 pl-6 border"
+            to="/"
+          >
+            HOME
+          </Link>
+          <Link
+            onClick={() => setVisible(false)}
+            className="py-2 pl-6 border"
+            to="/collection"
+          >
+            COLLECTION
+          </Link>
+          <Link
+            onClick={() => setVisible(false)}
+            className="py-2 pl-6 border"
+            to="/about"
+          >
+            ABOUT
+          </Link>
+          <Link
+            onClick={() => setVisible(false)}
+            className="py-2 pl-6 border"
+            to="/contact"
+          >
+            CONTACT
+          </Link>
         </div>
+      </div>
     </div>
   );
 };
